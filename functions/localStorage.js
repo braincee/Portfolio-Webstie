d30cbf45e4619b867563d60b184126a39ab7b0e1
telanet <benngetich2014@gmail.com>
2022-04-07 10:39:46 +0300

    Edit the local_storage.js

--- functions/localStorage.js
+++ functions/localStorage.js
@@ -13,13 +13,13 @@ formValue.addEventListener('input', () => {
   dataStorage.name = nameValue.value;
   dataStorage.email = emailValue.value;
   dataStorage.message = messageValue.value;
-  localStorage.setItem('UserData', JSON.stringify(dataStorage));
+  localStorage.setItem('userData', JSON.stringify(dataStorage));
 });
 
-if (localStorage.getItem('UserData') === null) {
-  localStorage.setItem('UserData', JSON.stringify(dataStorage));
+if (localStorage.getItem('userData') === null) {
+  localStorage.setItem('userData', JSON.stringify(dataStorage));
 } else {
-  const data = JSON.parse(localStorage.getItem('UserData'));
+  const data = JSON.parse(localStorage.getItem('userData'));
   nameValue.value = data.name;
   emailValue.value = data.email;
   messageValue.value = data.message;
